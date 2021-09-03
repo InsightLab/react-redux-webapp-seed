56a52cdc749176c5c076a2d4c2b36deff1f82097
Josué Machado <josueh.developer@gmail.com>
2021-09-03 17:27:44 -0300

    Atualizando api/Dummy.ts para consumir public/data/sample.json

--- src/services/api/Dummy.ts
+++ src/services/api/Dummy.ts
@@ -1,10 +1,8 @@
 import { AxiosPromise } from 'axios';
 import { api } from '..';
 
-
-
 export const ServiceApiDummy = {
   get: (): AxiosPromise<ApiSegChecker> => {
-    return api.get('https://seg.stage.mj.gov.br/api');
+    return api.get('/data/sample.json');
   },
 };
